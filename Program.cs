773b24aaa2f8b1dcb3956494d3f834f54d3c1b4c
hskartono <hskartono@gmail.com>
2024-11-20 18:00:56 +0700

    commit with running code example

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using RazorLight;
+
+namespace codegenerator_razor
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Definisikan model
+            var model = new YourModel
+            {
+                NamespaceName = "GeneratedNamespace",
+                ClassName = "GeneratedClass",
+                Properties = new List<PropertyModel>
+            {
+                new PropertyModel { Type = "int", Name = "Id" },
+                new PropertyModel { Type = "string", Name = "Name" }
+            }
+            };
+
+            var templatePath = "Template.cshtml";
+
+            // Baca template cshtml dari file
+            string template = System.IO.File.ReadAllText(templatePath);
+
+            // Buat engine RazorLight
+            var engine = new RazorLightEngineBuilder()
+                .UseFileSystemProject(Directory.GetCurrentDirectory())
+                .UseMemoryCachingProvider()
+                .Build();
+
+            // Render template dengan model
+            string result = await engine.CompileRenderAsync(templatePath, model);
+
+            // Tampilkan hasil atau simpan ke file
+            Console.WriteLine(result);
+            System.IO.File.WriteAllText("GeneratedClass.cs", result);
+        }
+    }
+}
---
+++ YourModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codegenerator_razor
+{
+    public class PropertyModel
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class YourModel
+    {
+        public string NamespaceName { get; set; }
+        public string ClassName { get; set; }
+        public List<PropertyModel> Properties { get; set; }
+    }
+
+}
